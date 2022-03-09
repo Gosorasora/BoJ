202b461450d02c785a0509f07d5cab0a47699c0e
Gosorasora <hanser0204@naver.com>
2022-03-09 14:17:52 +0900

    백준

---
+++ 2581.py
@@ -0,0 +1,28 @@
+#백준 2581 소수찾기
+import math
+
+def primenumber(x):
+    if x == 1:
+        return False
+    
+    for i in range (2, int(math.sqrt(x))+1):     
+        if x % i == 0:
+            return False
+    
+    return True
+
+M = int(input())
+N = int(input())
+sum = 0
+arr=[]
+
+for i in range (M , N+1):
+    if primenumber(i) == True:
+        sum = sum + i #합 찾기
+        arr.append(i)
+
+if sum == 0:
+    print(-1)
+else:
+    print(sum)
+    print(arr[0])
