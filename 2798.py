b3a24313a9525f917988dd359057a430e664cb49
MagicSora <93601786+Gosorasora@users.noreply.github.com>
2023-06-02 17:32:52 +0900

    BoJ

--- 2798.py
+++ 2798.py
@@ -3,4 +3,15 @@
 M, N = map(int, input().split())
 M_list = list(map(int, input().split()))
 
-# 배열의 1번 2번 3번 더하고 아니면 1 2 4 1 2 5
+result = 0
+
+for i in range(M):
+    for j in range(i+1, M):
+        for k in range(j+1, M):
+            
+            NewSum = M_list[i]+M_list[j]+M_list[k]
+            
+            if NewSum <= N:
+                result = max(result, NewSum)
+
+print(result)
